2df532a7e80e7078f56546409703d791d4fcf02b
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2019-06-09 03:54:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+<title>Kitabaca.co (Coming Soon)</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2{font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+p {line-height: 2}
+.bgimg, .bgimg2 {
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+.bgimg {background-image: url("ladun/img/landing.jpg")}
+.bgimg2 {background-image: url("/w3images/flowers.jpg")}
+</style>
+<body>
+
+<!-- Header / Home-->
+<header class="w3-display-container w3-wide bgimg w3-grayscale-min" id="home">
+  <div class="w3-display-middle w3-text-white w3-center">
+    <h1 class="w3-jumbo"></h1>
+    <h2></h2>
+    <h2><b></b></h2>
+  </div>
+</header>
+
+<!-- Navbar (sticky bottom) -->
+<div class="w3-bottom w3-hide-small">
+  <div class="w3-bar w3-white w3-center w3-padding w3-opacity-min w3-hover-opacity-off">
+    <a href="#home" style="width:25%" class="w3-bar-item w3-button">Home</a>
+    <a href="#us" style="width:25%" class="w3-bar-item w3-button">About</a>
+    <a href="#wedding" style="width:25%" class="w3-bar-item w3-button">Contact</a>
+    <a href="#rsvp" style="width:25%" class="w3-bar-item w3-button w3-hover-black">Feedback</a>
+  </div>
+</div>
+
+
+</body>
+</html>
